f7c2199124bf465996ffa84e82392aeaf7643c13
etouraille <eouard.touraille@gmail.com>
2022-12-13 14:54:27 +0100

    [] add a container for socket

--- restart_node.sh
+++ restart_node.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ps -aux | grep 'node serve.js' | awk '{ print $1}' | xargs kill -9
+ps -aux | grep 'node socket.js' | awk '{ print $1}' | xargs kill -9
 docker exec -i api-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config.js"
+docker exec -i socket-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config-socket.js"
