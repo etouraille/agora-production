804af490d314d49129d4d38a348e38d0e14a51f6
Ubuntu <ubuntu@vps-ddad2338.vps.ovh.net>
2022-12-14 05:17:23 +0000

    co

--- restart_node.sh
+++ restart_node.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ps -aux | grep 'pm2' | awk '{ print $1}' | xargs kill -9
+sudo ps -aux | grep 'node socket.js' | awk '{ print $2}' | xargs sudo kill -9
 docker exec -i api-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config.js"
-docker exec -i socket-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config-socket.js"
+#docker exec -i socket-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config-socket.js"
+docker exec -i socket-agora /bin/bash -c "node socket.js"
