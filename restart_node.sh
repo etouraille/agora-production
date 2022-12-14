832790fff94352985272f5d31ca36f21ff93a5b2
etouraille <eouard.touraille@gmail.com>
2022-12-14 13:11:04 +0100

    co

--- restart_node.sh
+++ restart_node.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 ps -aux | grep 'pm2' | awk '{ print $1}' | xargs kill -9
 sudo ps -aux | grep 'node socket.js' | awk '{ print $2}' | xargs sudo kill -9
-docker exec -i api-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config.js"
-#docker exec -i socket-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config-socket.js"
+docker exec -d api-agora /bin/bash -c "pm2 kill;pm2 start ecosystem.config.js"
 docker exec -d socket-agora /bin/bash -c "node socket.js"
 
 
